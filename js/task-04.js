aa09fa6eb88b37094fe6435a05a36c67cc1fdee7
Olzhas Ibrayev <95501759+ibraevsoft@users.noreply.github.com>
2022-10-17 00:46:27 +0600

    finalized-task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,17 @@
-let counterValue  = 0;
+let counterValue = 0;
 
-const startCounter = document.querySelector('.value');
+const сounter = document.querySelector("#value");
 
 const decrementFun = () => {
-    counter.textContent = counterValue -=1;
-}
+  сounter.textContent = counterValue -= 1;
+};
 
 const incrementFun = () => {
-    counter.textContent = counterValue +=1;
-}
+  сounter.textContent = counterValue += 1;
+};
 
 const dec = document.querySelector('button[data-action="decrement"]');
-dec.addEventListener('click', decrementFun)
+dec.addEventListener("click", decrementFun);
 
 const inc = document.querySelector('button[data-action="increment"]');
-inc.addEventListener('click', incrementFun)
+inc.addEventListener("click", incrementFun);
--- js/task-09.js
+++ js/task-09.js
@@ -2,13 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const bodyColor = document.querySelector('body');
-const btnChangeColor = document.querySelector('.change-color');
-const textColor = document.querySelector('.color');
+const bodyColor = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
 
-btnChangeColor.addEventListener('click', changeColor);
+btnChangeColor.addEventListener("click", changeColor);
 
-function changeColor(){
+function changeColor() {
   bodyColor.style.backgroundColor = getRandomHexColor();
   textColor.textContent = bodyColor.style.backgroundColor;
 }
